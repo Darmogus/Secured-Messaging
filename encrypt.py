ef9758ededd5c78bd0eabb4bb11586187781aea2
Darmogus <dark.dario06@gmail.com>
2025-02-17 16:10:00 +0100

    POO code encrypt

--- encrypt.py
+++ encrypt.py
@@ -3,77 +3,97 @@
 
 
 # --- Classes ---
-class Encrypt:
+class CipherBase:
     ALPHABET: str = "abcdefghijklmnopqrstuvwxyz"
-    ALPHABET_DICT: dict[str, int] = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25}
+    ALPHABET_DICT: dict[str, int] = {char: idx for idx, char in enumerate(ALPHABET)}
+
+    @staticmethod
+    def _shift_char(char: str, shift: int) -> str:
+        """Shift a character by a given number of positions in the alphabet."""
+        return CipherBase.ALPHABET[(CipherBase.ALPHABET_DICT[char] + shift) % 26]
+
+    @staticmethod
+    def _generate_vigenere_key(message: str, key: str) -> list[str]:
+        """Generate a Vigenère key that matches the length of the message."""
+        return [key[i % len(key)] for i in range(len(message))]
     
-    def __init__(self, message: str, key: str):
-        self.message = message
-        self.key = key
+
+class Encrypt(CipherBase):
+    @staticmethod
+    def cesar(message: str, key: str) -> str:
+        """Encrypt a message using the Caesar cipher."""
+        shift = int(key)
+        listMessage = []
+        for char in message:
+            encrypted_char = CipherBase._shift_char(char, shift)
+            listMessage.append(encrypted_char)
         
-    def cesar(self) -> str:
-        key: int = int(self.key)
-        messageList: list[str] = []
-        for char in self.message:
-            messageList.append(Encrypt.ALPHABET[(Encrypt.ALPHABET_DICT[char] + key) % 26])
+        encryptedMessage: str = "".join(listMessage)
+        return encryptedMessage
+
+    @staticmethod
+    def vigenere(message: str, key: str) -> str:
+        """Encrypt a message using the Vigenère cipher."""
+        vigenere_key = CipherBase._generate_vigenere_key(message, key)
+        listMessage = []
+        for message_char, key_char in zip(message, vigenere_key):
+            shift = CipherBase.ALPHABET_DICT[key_char]
+            encrypted_char = CipherBase._shift_char(message_char, shift)
+            listMessage.append(encrypted_char)
         
-        newMessage: str = "".join(messageList)
-        return newMessage
+        encryptedMessage: str = "".join(listMessage)
+        return encryptedMessage
+            
+            
+class Decrypt(CipherBase):
+    @staticmethod        
+    def cesar(message: str, key: str) -> str:
+        """Decrypt a message using the Caesar cipher."""
+        shift: int = int(key)
+        listMessage: str = []
+        for char in message:
+            decryptedChar: str = CipherBase._shift_char(char, -shift)
+            listMessage.append(decryptedChar)
+        
+        decryptedMessage: str = "".join(listMessage)
+        return decryptedMessage
     
-    def vigenere(self) -> str:
-        key: list[int] = []
-        for i in range(len(self.message)):
-            key.append(self.key[i % len(self.key)])
+    @staticmethod
+    def vigenere(message: str, key: str) -> str:
+        """Decrypt a message using the Vigenère cipher."""
+        vigenere_key = CipherBase._generate_vigenere_key(message, key)
+        listMessage = []
+        for message_char, key_char in zip(message, vigenere_key):
+            shift = -CipherBase.ALPHABET_DICT[key_char]
+            decrypted_char = CipherBase._shift_char(message_char, shift)
+            listMessage.append(decrypted_char)
         
-        messageList: list[str] = []
-        for messageChar, keyChar in zip(self.message, key):
-            messageCharIndex: int = Encrypt.ALPHABET_DICT[messageChar]
-            keyCharIndex: int = Encrypt.ALPHABET_DICT[keyChar]
-            
-            messageList.append(Encrypt.ALPHABET[(messageCharIndex + keyCharIndex) % 26])
-          
-        newMessage: str = "".join(messageList)
-        return newMessage
-            
-            
-        
-        
-class Decrypt:
-    ALPHABET: str = "abcdefghijklmnopqrstuvwxyz"
-    ALPHABET_DICT: dict[str, int] = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25}
-    
-    def __init__(self, message: str, key: str):
-        self.message = message
-        self.key = key
-        
-    def cesar(self) -> str:
-        key: int = int(self.key)
-        newMessage: str = []
-        for char in self.message:
-            newMessage.append(Decrypt.ALPHABET[(Decrypt.ALPHABET_DICT[char] - key) % 26])
-        
-        newMessage = "".join(newMessage)
-        return newMessage
-    
-    def vigenere(self) -> str:
-        key: list[int] = []
-        for i in range(len(self.message)):
-            key.append(self.key[i % len(self.key)])
-        
-        messageList: list[str] = []
-        for messageChar, keyChar in zip(self.message, key):
-            messageCharIndex: int = Decrypt.ALPHABET_DICT[messageChar]
-            keyCharIndex: int = Decrypt.ALPHABET_DICT[keyChar]
-            
-            messageList.append(Decrypt.ALPHABET[(messageCharIndex - keyCharIndex) % 26])
-          
-        newMessage: str = "".join(messageList)
-        return newMessage
+        decryptedMessage: str = "".join(listMessage)
+        return decryptedMessage
         
         
 # --- Main ---
 def main():
-    pass
-    
+    message = "hello"
+    cesar_key = "3"
+    vigenere_key = "key"
+
+    # Encrypt using Caesar cipher
+    encrypted_cesar = Encrypt.cesar(message, cesar_key)
+    print(f"Encrypted with Caesar: {encrypted_cesar}")
+
+    # Decrypt using Caesar cipher
+    decrypted_cesar = Decrypt.cesar(encrypted_cesar, cesar_key)
+    print(f"Decrypted with Caesar: {decrypted_cesar}")
+
+    # Encrypt using Vigenère cipher
+    encrypted_vigenere = Encrypt.vigenere(message, vigenere_key)
+    print(f"Encrypted with Vigenère: {encrypted_vigenere}")
+
+    # Decrypt using Vigenère cipher
+    decrypted_vigenere = Decrypt.vigenere(encrypted_vigenere, vigenere_key)
+    print(f"Decrypted with Vigenère: {decrypted_vigenere}")
+
 if __name__ == "__main__":
     main()
+    
