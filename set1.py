b5a4ba3bebd6c8f4393912c0ae6bfbd235f4ef56
Darmogus <dark.dario06@gmail.com>
2025-03-03 10:13:42 +0100

    code cleaning

--- set1.py
+++ set1.py
@@ -11,18 +11,18 @@ class Challenge:
 class Challenge1(Challenge):
     """Hex to Base64"""
     BASE64_TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-    def __init__(self, hexString: str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"):
+    def __init__(self, hex_string: str):
         super().__init__(1)
-        binList: list[str] = self.convert_to_bin(hexString)
+        binList: list[str] = self.convert_to_bin(hex_string)
         byteList: list[str] = self.convert_to_bytes(binList)
         base64String: str = self.convert_to_base64(byteList)
         
-        print(f"Hex string = {hexString}")
+        print(f"Hex string = {hex_string}")
         print(f"Base64 string = {base64String}")
 
-    def convert_to_bin(self, hexString: str) -> list[str]:
+    def convert_to_bin(self, hex_string: str) -> list[str]:
         binList: list[str] = []
-        for hexChar in hexString:
+        for hexChar in hex_string:
             binValue = bin(int(hexChar, 16))[2:].zfill(4)
             binList.append(binValue)
         return binList
@@ -53,25 +53,25 @@ class Challenge1(Challenge):
 
 class Challenge2(Challenge):
     """Fixed XOR"""
-    def __init__(self, hexString1: str = "1c0111001f010100061a024b53535009181c", hexString2: str = "686974207468652062756c6c277320657965"):
+    def __init__(self, hex_string_1: str, hex_string_2: str):
         super().__init__(2)
-        hexOutput = self.xor(hexString1, hexString2)
+        hexOutput = self.xor(hex_string_1, hex_string_2)
         
         print(hexOutput)
         
-    def xor(self, hexString1, hexString2) -> str:
-        int1 = int(hexString1, 16)
-        int2 = int(hexString2, 16)
+    def xor(self, hex_string_1, hex_string_2) -> str:
+        int1 = int(hex_string_1, 16)
+        int2 = int(hex_string_2, 16)
         
         xorResult = int1 ^ int2
         xorHex = hex(xorResult)[2:]  # Removing 0x
-        xorHex = xorHex.zfill(len(hexString1)) # Padding
+        xorHex = xorHex.zfill(len(hex_string_1)) # Padding
         
         return xorHex
 
 
 class Challenge3(Challenge):
-    def __init__(self, hexStr = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736", usedElsewhere = False):
+    def __init__(self, usedElsewhere: bool = False):
         super().__init__(3) if not usedElsewhere else None
         self.hexStr = hexStr
         
@@ -109,21 +109,21 @@ class Challenge3(Challenge):
                
 
 class Challenge4(Challenge):
-    def __init__(self, dataFile: str = "set1_chall4_data.txt"):
+    def __init__(self, data_file: str):
         super().__init__(4)
         try:
-            with open(dataFile, 'r') as file:
+            with open(data_file, 'r') as file:
                 for line in file:
                     challenge = Challenge3(line, True)
                     result = challenge.decrypt_xor_cipher()
                     if result[0] is not None:
                         print(result)
         except FileNotFoundError:
-            print(f"Erreur : fichier {dataFile} introuvable.")
+            print(f"Erreur : fichier {data_file} introuvable.")
             
 
 class Challenge5(Challenge):
-    def __init__(self, plaintext, key):
+    def __init__(self, plaintext: str, key: str):
         super().__init__(5)
         self.plaintext = plaintext
         self.key = key
@@ -141,7 +141,7 @@ class Challenge5(Challenge):
     
 
 class Challenge6(Challenge):
-    def __init__(self, file_path):
+    def __init__(self, file_path: str):
         super().__init__(6)
         self.file_path = file_path
         self.ciphertext = self._load_and_decode_file()
@@ -184,10 +184,10 @@ class Challenge6(Challenge):
 
 
 class Challenge7(Challenge):
-    def __init__(self, key):
+    def __init__(self, data_file: str, key: str):
         super().__init__(7)
         self.key = key.encode()  # Convertir la clé en bytes
-        self.cipherText = self.load_base64_file("set1_chall7_data.txt")
+        self.cipherText = self.load_base64_file(data_file)
         self.plainText = self.decrypt_aes_ecb(self.cipherText)
         
         print(self.plainText)
@@ -205,9 +205,9 @@ class Challenge7(Challenge):
 
 
 class Challenge8(Challenge):
-    def __init__(self, filePath: str = "set1_chall8_data.txt"):
+    def __init__(self, file_path: str):
         super().__init__(8)
-        self.filePath = filePath
+        self.file_path = file_path
         
         ciphertexts = self.read_hex_file()
         index, ecb_ciphertext = self.detect_ecb_cipher(ciphertexts)
@@ -234,7 +234,7 @@ class Challenge8(Challenge):
     def read_hex_file(self) -> list:
         # Lire chaque ligne du fichier et décoder les hex
         ciphertexts = []
-        with open(self.filePath, 'r') as file:
+        with open(self.file_path, 'r') as file:
             for line in file:
                 line = line.strip()  # Enlever les espaces ou les retours à la ligne
                 if line:  # Si la ligne n'est pas vide
@@ -246,14 +246,14 @@ class Challenge8(Challenge):
 
 
 def main():
-    Challenge1()
-    Challenge2(hexString1="1c0111001f010100061a024b53535009181c", hexString2="686974207468652062756c6c277320657965")
-    Challenge3().decrypt_xor_cipher()
-    Challenge4()
-    Challenge5("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal", "ICE").repeating_key_xor()
-    Challenge6("set1_chall6_data.txt")
-    Challenge7("YELLOW SUBMARINE").decrypt_aes_ecb(Challenge7.load_base64_file("set1_chall7_data.txt"))
-    Challenge8()
+    Challenge1(hex_string="49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+    Challenge2(hex_string_1="1c0111001f010100061a024b53535009181c", hex_string_2="686974207468652062756c6c277320657965")
+    Challenge3(hex_string="1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+    Challenge4(data_file="set1_chall4_data.txt")
+    Challenge5(plaintext="Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal", key="ICE").repeating_key_xor()
+    Challenge6(file_path="set1_chall6_data.txt")
+    Challenge7(data_file="set1_chall7_data.txt", key="YELLOW SUBMARINE")
+    Challenge8(file_path="set1_chall8_data.txt")
     
 
 if __name__ == "__main__":
